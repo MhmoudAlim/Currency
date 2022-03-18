f0fd302c85a8395f34d2d3dff78f756cf948eec3
mahmoud.hussein@gen-c.com <*Hp&bh3on6ebsPQMDuWwF>
2022-03-18 21:14:37 +0200

    implement the modules injection and add all layer needed dependencies

--- buildSrc/src/main/java/Retrofit.kt
+++ buildSrc/src/main/java/Retrofit.kt
@@ -5,7 +5,6 @@
 object Retrofit {
     private const val version = "2.9.0"
     const val retrofit = "com.squareup.retrofit2:retrofit:$version"
-    const val gson = "com.google.code.gson:gson:$version"
 
     private const val okHttpVersion = "5.0.0-alpha.2"
     const val okHttp = "com.squareup.okhttp3:okhttp:$okHttpVersion"
