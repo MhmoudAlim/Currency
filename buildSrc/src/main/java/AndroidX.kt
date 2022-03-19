5e2ac37456a199c15ae1bbf7c278aa10347f7672
mahmoud.hussein@gen-c.com <*Hp&bh3on6ebsPQMDuWwF>
2022-03-19 12:18:54 +0200

    add Navigation component dependencies

--- buildSrc/src/main/java/AndroidX.kt
+++ buildSrc/src/main/java/AndroidX.kt
@@ -11,4 +11,9 @@ object AndroidX {
     private const val constraintLayoutVersion = "2.1.3"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"
 
+    private const val nav_version = "2.4.1"
+    const val fragmentNavigation =  "androidx.navigation:navigation-fragment-ktx:$nav_version"
+    const val uiNavigation =  "androidx.navigation:navigation-ui-ktx:$nav_version"
+    const val safeArgs =  "androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version"
+
 }
--- buildSrc/src/main/java/Kotlin.kt
+++ buildSrc/src/main/java/Kotlin.kt
@@ -2,5 +2,5 @@
  * Created by Mahmoud Alim on 18/03/2022.
  */
 object Kotlin {
-    const val version = "1.5.21"
+    const val version = "1.6.10"
 }
