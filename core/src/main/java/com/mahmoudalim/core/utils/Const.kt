30cea732163d0514dd1c001b9a1378f12af80cf7
mahmoud.hussein@gen-c.com <*Hp&bh3on6ebsPQMDuWwF>
2022-03-21 01:47:16 +0200

    add Const.kt object and add usesCleartextTraffic in app manifest to handle http api base url

---
+++ core/src/main/java/com/mahmoudalim/core/utils/Const.kt
@@ -0,0 +1,10 @@
+package com.mahmoudalim.core.utils
+
+/**
+ * Created by Mahmoud Alim on 19/03/2022.
+ */
+object Const {
+
+    const val BASE_URL = "http://data.fixer.io/"
+    const val API_KEY = "2142ff11698c00bc28e8ee359ac9f4e5"
+}
