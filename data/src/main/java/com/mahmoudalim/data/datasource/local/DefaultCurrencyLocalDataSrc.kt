5fad637ac730e9c6f22552c89b21fc1dd62467bf
MhmoudAlim <mahmoudhusseinalim@gmail.com>
2022-03-22 00:47:58 +0200

    refactor DefaultCurrencyLocalDataSrc
    add funcs implenetation of insertConversionRecord() and fetchConversionsHistoryList() ,inject histroyDao in the constructor to handle accessing local data src in default currency repo

--- data/src/main/java/com/mahmoudalim/data/datasource/local/DefaultCurrencyLocalDataSrc.kt
+++ data/src/main/java/com/mahmoudalim/data/datasource/local/DefaultCurrencyLocalDataSrc.kt
@@ -1,10 +1,23 @@
 package com.mahmoudalim.data.datasource.local
 
+import com.mahmoudalim.data.database.HistoryDao
+import com.mahmoudalim.data.database.HistoryEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
 /**
  * Created by Mahmoud Alim on 19/03/2022.
  */
-class DefaultCurrencyLocalDataSrc : CurrencyLocalDataSrc {
 
-    override suspend fun fetchData() {
+class DefaultCurrencyLocalDataSrc @Inject constructor(
+    private val historyDao: HistoryDao
+) : CurrencyLocalDataSrc {
+    override suspend fun insertConversionRecord(historyEntity: HistoryEntity) {
+        historyDao.insertConversionRecord(historyEntity)
     }
+
+    override suspend fun fetchConversionsHistoryList(): Flow<List<HistoryEntity>> {
+        return historyDao.fetchAllRecords()
+    }
+
 }
