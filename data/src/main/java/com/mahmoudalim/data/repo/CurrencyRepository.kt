0d8f776f1423f43505c40b601703b287a9be8a52
MhmoudAlim <mahmoudhusseinalim@gmail.com>
2022-03-22 00:45:05 +0200

    add abstract funcs to manage interacting with local database in base CurrencyRepository interface

--- data/src/main/java/com/mahmoudalim/data/repo/CurrencyRepository.kt
+++ data/src/main/java/com/mahmoudalim/data/repo/CurrencyRepository.kt
@@ -1,7 +1,9 @@
 package com.mahmoudalim.data.repo
 
 import com.mahmoudalim.core.utils.AppResponse
+import com.mahmoudalim.data.database.HistoryEntity
 import com.mahmoudalim.data.models.CurrencyResponse
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Created by Mahmoud Alim on 19/03/2022.
@@ -10,4 +12,6 @@ import com.mahmoudalim.data.models.CurrencyResponse
 interface CurrencyRepository {
 
     suspend fun getRates(base: String, key : String) : AppResponse<CurrencyResponse>
+    suspend fun fetchConversionsHistoryList(): Flow<List<HistoryEntity>>
+    suspend fun insertConversionRecord(historyEntity: HistoryEntity)
 }
