dee5bc23eae2744e5962444c3e0743a8adaccc0f
MhmoudAlim <mahmoudhusseinalim@gmail.com>
2022-03-22 00:44:02 +0200

    add abstract funcs to manage interacting with database in CurrencyLocalDataSrc interface

--- data/src/main/java/com/mahmoudalim/data/datasource/local/CurrencyLocalDataSrc.kt
+++ data/src/main/java/com/mahmoudalim/data/datasource/local/CurrencyLocalDataSrc.kt
@@ -1,9 +1,13 @@
 package com.mahmoudalim.data.datasource.local
 
+import com.mahmoudalim.data.database.HistoryEntity
+import kotlinx.coroutines.flow.Flow
+
 /**
  * Created by Mahmoud Alim on 19/03/2022.
  */
 interface CurrencyLocalDataSrc {
 
-    suspend fun fetchData ()
+    suspend fun insertConversionRecord(historyEntity: HistoryEntity)
+    suspend fun fetchConversionsHistoryList(): Flow<List<HistoryEntity>>
 }
