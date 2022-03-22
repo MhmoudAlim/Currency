c2d00a6421fd1b15da140d61254578f97e33d523
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-03-22 22:12:29 +0200

    implement fun to get the last threedays including today in AppDate.kt

--- core/src/main/java/com/mahmoudalim/core/date/AppDate.kt
+++ core/src/main/java/com/mahmoudalim/core/date/AppDate.kt
@@ -19,13 +19,22 @@ object AppDate {
         return date.format(DateTimeFormatter.ofPattern(format.value))
     }
 
-
     fun format(
         milli: Long,
         format: Format,
     ): String {
         val date = LocalDateTime.ofInstant(Instant.ofEpochMilli(milli), ZoneId.systemDefault())
         return date.format(DateTimeFormatter.ofPattern(format.value))
+    }
+
+    fun pastDaysOf(days: Int): List<String> {
+        var date = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault())
+        val daysItems = mutableListOf<String>()
+        for (i in 0 until days) {
+            daysItems.add(date.format(DateTimeFormatter.ofPattern(Format.DD_MM_YYYY.value)))
+            date = date.minusDays(1)
+        }
+        return daysItems
     }
 
 }
