e9ebe5bf2b13d0b97c7cd73842fc31721f060fb4
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-03-24 14:18:59 +0200

    add snacBar() fun to showSnackbar Events

---
+++ app/src/main/java/com/mahmoudalim/currency/utils/Snackbar.kt
@@ -0,0 +1,29 @@
+package com.mahmoudalim.currency.utils
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import com.google.android.material.snackbar.Snackbar
+import com.mahmoudalim.currency.R
+
+/**
+ * Created by Mahmoud Alim on 24/03/2022.
+ */
+
+
+fun snackBar(
+    view: View,
+    @SuppressLint("ResourceAsColor") @ColorInt textColor: Int =  R.color.white,
+    @SuppressLint("ResourceAsColor") @ColorInt bgColor: Int = R.color.purple_700,
+    message: String,
+) {
+    val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+    val snackbarView = snackbar.view
+    snackbarView.setBackgroundColor(bgColor)
+    val textView =
+        snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
+    textView.setTextColor(textColor)
+    snackbar.show()
+}
+
