9487d700f9e7cbd9e325194dd4daf792afb6747c
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-03-24 17:57:31 +0200

    introduce DomainModule.kt and provide UseCases

---
+++ data/src/main/java/com/mahmoudalim/data/di/DomainModule.kt
@@ -0,0 +1,33 @@
+package com.mahmoudalim.data.di
+
+import com.mahmoudalim.data.repo.CurrencyRepository
+import com.mahmoudalim.data.usecase.CurrencyUseCases
+import com.mahmoudalim.data.usecase.FetchConversionsHistoryListUseCase
+import com.mahmoudalim.data.usecase.GetAllRatesUseCase
+import com.mahmoudalim.data.usecase.InsertConversionRecordUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+/**
+ * Created by Mahmoud Alim on 24/03/2022.
+ */
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class DomainModule {
+
+    @ViewModelScoped
+    @Provides
+    fun provideConverterUseCases(
+        repository: CurrencyRepository,
+    ): CurrencyUseCases {
+        return CurrencyUseCases(
+            insertConversionRecordUseCase = InsertConversionRecordUseCase(repository),
+            fetchConversionsHistoryUseCase = FetchConversionsHistoryListUseCase(repository),
+            getAllRatesUseCase = GetAllRatesUseCase(repository)
+        )
+    }
+}
